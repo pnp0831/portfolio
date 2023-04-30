e640034e9c66e2d043baee987f7dea5077440b7e
Pam <pam@192.168.2.42>
2023-04-30 21:41:14 +0700

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+let nextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  poweredByHeader: false,
+};
+
+// if (process.env.ANALYZE) {
+//   const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: true,
+//   });
+
+//   nextConfig = withBundleAnalyzer(nextConfig);
+// }
+
+module.exports = nextConfig;
